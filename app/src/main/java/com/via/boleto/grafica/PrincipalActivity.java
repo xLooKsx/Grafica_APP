a3d09e94f9e54fb73ad22d3b060d0d4915ab08bc
LooKs <lu.oliveira.silva98@gmail.com>
2018-03-30 23:06:33 -0300

    Tela Principal feita e pequenas mudanças no projeto

---
+++ app/src/main/java/com/via/boleto/grafica/PrincipalActivity.java
@@ -0,0 +1,155 @@
+package com.via.boleto.grafica;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class PrincipalActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private FragmentTransaction fragmentTransaction;
+    private NavigationView navigationView;
+    private int idMenu = R.id.nav_dinheiro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_principal, new DinheiroFragment());
+        fragmentTransaction.commit();
+
+        navigationView.setCheckedItem(R.id.nav_dinheiro);
+    }
+
+    public void setActionBarTitle(String title){
+
+        getSupportActionBar().setTitle(title);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        Random r = new Random();
+        TextView txtViewNovoValor;
+        int novoValor;
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_atualizar) {
+
+            switch (idMenu){
+
+                case R.id.nav_dinheiro:
+                    txtViewNovoValor = (TextView)findViewById(R.id.txt_vlrdinheiro);
+                    novoValor = r.nextInt(800 - 10) + 65;
+                    txtViewNovoValor.setText("R$ "+String.valueOf(novoValor)+",00");
+                    break;
+
+                case R.id.nav_cartao_debito:
+                    txtViewNovoValor = (TextView)findViewById(R.id.txt_vlrCartaoDebito);
+                    novoValor = r.nextInt(400 - 10) + 65;
+                    txtViewNovoValor.setText("R$ "+String.valueOf(novoValor)+",00");
+                    break;
+
+                case R.id.nav_cartao_credito:
+                    txtViewNovoValor = (TextView)findViewById(R.id.txt_vlrCartaoCredito);
+                    novoValor = r.nextInt(328 - 10) + 65;
+                    txtViewNovoValor.setText("R$ "+String.valueOf(novoValor)+",00");
+                    break;
+            }
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+
+        switch (id){
+
+            case R.id.nav_dinheiro:
+                fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frame_principal, new DinheiroFragment());
+                fragmentTransaction.commit();
+                break;
+
+            case R.id.nav_cartao_debito:
+                fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frame_principal, new CartaoDebitoFragment());
+                fragmentTransaction.commit();
+                break;
+
+            case R.id.nav_cartao_credito:
+                fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frame_principal, new CartaoCreditoFragment());
+                fragmentTransaction.commit();
+                break;
+
+            case R.id.nav_config_email:
+                fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frame_principal, new ConfigEmailFragment());
+                fragmentTransaction.commit();
+                break;
+
+            case R.id.nav_sair:
+                Intent intent = new Intent(PrincipalActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        idMenu = id;
+        return true;
+    }
+}
